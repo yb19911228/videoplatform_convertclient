e2a2dba177993c0d1c4eb3e82435f9d88ee713ee
yb19911228 <yb19911228@gmail.com>
2013-11-24 21:56:17 +0800

    First Version
    Have convertvideo Problem。Thread etc。

---
+++ videoplatform_convertclient/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace videoplatform_convertclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "数据库设置";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "地址：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "用户名：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "密码：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "数据库名：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(340, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "文件输出目录：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(340, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "文件输出目录：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(340, 10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "文件目录设置";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 25);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "192.168.1.170";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 74);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 25);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "root";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 104);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(161, 25);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "123456";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 135);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(161, 25);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "search2";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(342, 74);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(268, 25);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "x:\\";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(342, 135);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(268, 25);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.Text = "x:\\";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 177);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 19);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "注册";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 177);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 19);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "重置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(451, 177);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 21);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "下线";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(41, 217);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 15);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "本机信息";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(41, 249);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 15);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "未注册";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(340, 217);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 15);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "任务信息";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(342, 248);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(22, 15);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "无";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(343, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "上线";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(496, 24);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 404);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ videoplatform_convertclient/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using MySQLDriverCS;
+using System.Diagnostics;
+namespace videoplatform_convertclient
+{
+    public partial class Form1 : Form
+    {
+        public string DB_Address, DB_User, DB_Pass, DB_Database, File_Input, File_Output, Host_ID, Host_IP, Host_Name;
+        public string Host_Status="0";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //数据库连接参数初始化
+        public void DB_set()
+        {
+            DB_Address = textBox1.Text;
+            DB_User = textBox2.Text;
+            DB_Pass = textBox3.Text;
+            DB_Database = textBox4.Text;
+            File_Input = textBox5.Text;
+            File_Output = textBox6.Text;
+            Host_ID = ReadTxt();
+        }
+        //注册按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //读取输入的数据库地址、用户、密码、数据库和转码文件输入输出目录
+            DB_Address = textBox1.Text;
+            DB_User = textBox2.Text;
+            DB_Pass = textBox3.Text;
+            DB_Database = textBox4.Text;
+            File_Input = textBox5.Text;
+            File_Output = textBox6.Text;
+            Host_Name = GetHostName();//获取本机计算名
+            Host_IP = GetHostIP();//获取本机IP地址
+            if (MysqlTest() == false)//测试数据库是否连接成功 
+            {
+                MessageBox.Show("数据库连接错误，请检查参数");
+            }
+            //检查是否已经注册，若还未注册则注册并上线，若已经注册则扫描任务
+            else if (ReadTxt() == null)
+            {
+                //向数据库插入主机信息，并更新ID信息
+                MessageBox.Show("正在注册");
+                //向数据库写入信息
+                string InsertQuery = "INSERT INTO chost(chostname,chostip,choststatus)VALUES"+"('"+Host_Name+"','"+Host_IP+"','0')";//注册语句，字段名不需要引号
+                MysqlWrite(InsertQuery);
+                //返回ID
+                string SelectQuery = "SELECT chostid FROM chost WHERE chostname='" + Host_Name + "' AND chostip='" + Host_IP + "'";
+                //MessageBox.Show(SelectQuery);
+                Host_ID = MysqlRead(SelectQuery);
+                WriteTxt(Host_ID);//将ID信息写入文件
+            }
+            else
+            {
+                string SelectQuery = "SELECT choststatus FROM chost WHERE chostname='" + Host_Name + "' AND chostip='" + Host_IP + "'";
+                Host_Status = MysqlRead(SelectQuery);
+                if (Host_Status == "0")
+                    MessageBox.Show("本机已经注册，但未上线，请点击上线按钮！");
+                else
+                    MessageBox.Show("本机已运行！");
+            }
+
+        }
+        //点击重置按钮，清空输入信息
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        //数据库连接测试
+        public Boolean MysqlTest()
+        {
+            Boolean ReturnValue;
+            MySQLConnection conn = null;
+            conn = new MySQLConnection(new MySQLConnectionString(DB_Address, DB_Database, DB_User, DB_Pass).AsString);
+            /* try
+             {
+                 //创建一个TcpClient对象，设置连接服务器的IP和端口号
+                 TcpClient myclient = new TcpClient(DB_Address, 3306);
+                    
+             }
+             catch
+             {
+                 ReturnValue = false;
+             }*/
+            try
+            {
+                conn.Open();
+                HostInfo();
+                ReturnValue = true;
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show("数据库连接失败，请检查参数");
+                ReturnValue = false;
+            }
+            conn.Close();
+            ReturnValue = true;
+
+            return ReturnValue;
+        }
+
+        //数据库读取
+        public string MysqlRead(string Query)
+        {
+            string ReturnValue = null;
+            MySQLConnection conn = null;
+            //MySQLCommand commn = new MySQLCommand("set names utf-8", conn);
+            conn = new MySQLConnection(new MySQLConnectionString(DB_Address, DB_Database, DB_User, DB_Pass).AsString);
+            //string query = "select * from video";
+            conn.Open();
+            MySQLCommand cmd = new MySQLCommand(Query, conn);
+           
+            MySQLDataReader reader = cmd.ExecuteReaderEx();
+            while (reader.Read())
+            {
+                ReturnValue = reader.GetString(0);
+            }
+            conn.Close();
+            return ReturnValue;
+
+        }
+
+        //数据库写入
+        public void MysqlWrite(string Query)
+        {
+            MySQLConnection conn = null;
+            //MySQLCommand commn = new MySQLCommand("set names utf-8", conn);
+            conn = new MySQLConnection(new MySQLConnectionString(DB_Address, DB_Database, DB_User, DB_Pass).AsString);
+            //string query = "select * from video";
+            MySQLCommand cmd = new MySQLCommand(Query, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();//执行数据库代码
+            conn.Close();
+        }
+
+        //更改状态信息
+        public void HostInfo()
+        {
+            if (Host_Status == "0")
+                Host_Status = "已下线";
+            else if (Host_Status == "9")
+                Host_Status = "下线中";
+            else
+                Host_Status = "已上线";
+            //显示本机信息
+            label10.Text = "本机ID：" + Host_ID.ToString() + "\n" +
+                           "数据库IP:" + DB_Address.ToString() + "\n" +
+                           "数据库用户：" + DB_User.ToString() + "\n" +
+                           "数据库名：" + DB_Database.ToString() + "\n" +
+                           "输入目录：" + File_Input.ToString() + "\n" +
+                           "输出目录：" + File_Output.ToString() + "\n" +
+                           "运行状态：" + Host_Status.ToString();
+        }
+
+        //读取配置文件
+        public string ReadTxt()
+        {
+            string ReturnValue;
+            try
+            {
+                //创建新的StreamReader对象
+                StreamReader SR = new StreamReader(@"config.txt");
+                //读取数据放入ConfigData中
+                string ConfigData = SR.ReadToEnd();
+                if (ConfigData != "")
+                    //如果配置文件不为空，则表示已经注册
+                    //MessageBox.Show("已注册"+ConfigData);
+                    ReturnValue = ConfigData;
+                else
+                    //如果配置文件为空，则连接数据库申请ID后写入
+                    //MessageBox.Show("配置文件为空");
+                    ReturnValue = null;
+                SR.Close();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("配置文件读取错误！");
+                return "error";
+            }
+            return ReturnValue;
+        }
+
+        //写入配置文件
+        public void WriteTxt(string OriginalHostId)
+        {
+            try
+            {
+                //保留文件现有数据，追加方式打开 confif.txt文件
+                StreamWriter SW = new StreamWriter(@"config.txt", true);
+                //向文件写入新的字符串，并关闭StreamWriter
+                SW.WriteLine(OriginalHostId);
+                SW.Close();
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("配置文件写入异常");
+                return;
+            }
+            MessageBox.Show("已注册，ID号为：" + Host_ID);
+            return;
+
+        }
+
+        //用户获取主机名和IP地址
+        public string GetHostName()
+        {
+            string HostName;
+            HostName = Dns.GetHostName();
+            return HostName;
+        }
+
+        public string GetHostIP()
+        {
+            string HostIP = null, HostName;
+            HostName = Dns.GetHostName();
+            System.Net.IPAddress[] addressList = Dns.GetHostByName(HostName).AddressList;
+            foreach (IPAddress ip in addressList)
+            {
+                HostIP = ip.ToString();
+            }
+            return HostIP;
+        }
+        
+        //上线按钮
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DB_set();
+            string SelectQuery = "SELECT choststatus FROM chost WHERE chostid='" + Host_ID + "'";
+            string Online = "UPDATE chost SET choststatus='1'WHERE chostid='" + Host_ID + "'";
+            Host_Status = MysqlRead(SelectQuery);
+            if (Host_Status == "0")
+            //修改信息上线
+            {
+                MysqlWrite(Online);
+                MessageBox.Show("本机已上线运行");
+                Host_Status = "1";
+                //开启转码线程
+                Thread t = new Thread(new ThreadStart(ConvertVideo)); //创建Thread对象
+                t.Start();//开始线程
+
+                //ConvertVideo();
+            }
+            else
+            {
+                MessageBox.Show("本机已上线运行！");
+                ConvertVideo();
+            }
+            HostInfo();//更新状态信息
+        }
+
+        //下线按钮
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DB_set();
+            string SelectQuery = "SELECT choststatus FROM chost WHERE chostid='" + Host_ID + "'";
+            string Offline = "UPDATE chost SET choststatus='0'WHERE chostid='" + Host_ID + "'";
+            Host_Status = MysqlRead(SelectQuery);
+            if (Host_Status == "0")
+                MessageBox.Show("本机已下线！");
+            else
+            //修改信息下线
+            {
+                MysqlWrite(Offline);
+                //MessageBox.Show("本机已下线！");
+                Host_Status = "9";
+                //label12.Text = "无";
+            }
+            HostInfo();//更新状态信息
+        }
+
+
+        //运行转码程序
+        public void ConvertVideo()
+        {
+            DB_set();
+            //搜索状态表
+            string SelectQuery = "SELECT choststatus FROM chost WHERE chostid='" + Host_ID + "'";
+           Host_Status = MysqlRead(SelectQuery);
+            //MessageBox.Show(Host_Status);
+            if (Host_Status == "0")
+            {
+                //label12.Text = "已下线";
+                MessageBox.Show("已下线");
+                //Thread.Sleep(0);//阻止当前线程
+            }
+            else if (Host_Status == "1")
+            {
+                //等待一段时间继续运行转码函数
+                //label12.Text = "正在检索转码任务...";
+                Thread.Sleep(2000);
+                ConvertVideo();
+            }
+            else
+            {
+                MessageBox.Show("本机已运行！");
+            }
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            string Cmd = "ffmpeg.exe -i x:/test.flv  x:/1muh1.avi";
+            //Cmd = "ipconfig";
+              RunCmd(Cmd);
+        }
+
+        private void RunCmd (string Command)
+        {
+            //实例一个Process类，启动独立进程
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";//设定程序名
+            p.StartInfo.Arguments = "/c" + Command;//设定启动参数
+            p.StartInfo.UseShellExecute = false;//关闭shell的使用
+            p.StartInfo.RedirectStandardInput = true;//重定向标准输入
+            p.StartInfo.RedirectStandardOutput = true;//重定向标准输出
+            p.StartInfo.RedirectStandardError = true;//重定向错误输出
+            //p.StartInfo.CreateNoWindow = true;//设置不显示窗口
+             p.Start();//启动
+             //p.StandardInput.WriteLine(Command);//也可以用这种方式输入要执行的命令
+            //p.StandardInput.WriteLine("exit");//防止死机
+            //return p.StandardOutput.ReadLine();//从输出流取得命令执行结果
+        }
+        
+        
+    }
+}
